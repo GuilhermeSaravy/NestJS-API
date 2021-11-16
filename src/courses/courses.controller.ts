ab75104676cccc8d6262e6937673c0116da6022a
Guilherme Saravi <guilherme-saravy@hotmail.com>
2021-11-16 15:07:14 -0400

    Criando service de courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,16 +1,22 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
+  Put,
   Res,
 } from '@nestjs/common';
+import { CourseService } from './course.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CourseService) {}
+
   @Get()
   findAll(@Res() response) {
     return response.status(200).send('Listagem de cursos');
@@ -25,5 +31,15 @@ export class CoursesController {
   @HttpCode(HttpStatus.NO_CONTENT)
   creat(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Atualização do Curso #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id) {
+    return `Exclusão do Curso #${id}`;
   }
 }
